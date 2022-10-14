19e78a211786df405285a8db78fd8e6c680b7b49
Renato Zanella <renatozr07@gmail.com>
2022-10-14 20:21:46 -0300

    feat: req4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    nums.sort()
+
+    for i in range(len(nums) - 1):
+        j = i + 1
+
+        num1 = nums[i]
+        num2 = nums[j]
+
+        if not isinstance(num1, int) or num1 < 0:
+            return False
+
+        if num1 == num2:
+            return num1
+
+    return False
