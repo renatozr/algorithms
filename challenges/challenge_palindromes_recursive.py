fe27ae7ae3cbc87178e465ced2347028aaa680e3
Renato Zanella <renatozr07@gmail.com>
2022-10-14 17:41:47 -0300

    feat: req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+def is_palindrome_recursive(word, low_index=0, high_index=None):
+    if len(word) == 0:
+        return False
+
+    if high_index is None:
+        high_index = len(word) - 1
+
+    if low_index > high_index:
+        return True
+
+    return word[low_index] == word[high_index] and is_palindrome_recursive(
+        word, low_index + 1, high_index - 1
+    )
