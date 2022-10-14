dfddcc1b808e5195180f17f8e4a27bcc0b574ccb
Renato Zanella <renatozr07@gmail.com>
2022-10-14 17:27:31 -0300

    feat: req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for t1, t2 in permanence_period:
+        if not (isinstance(t1, int) and isinstance(t2, int)):
+            return None
+        if t1 <= target_time <= t2:
+            count += 1
+
+    return count
